1ef4005d8fdcdd6abece1d10327ab252226def7e
faomg201 <69358628+faomg201@users.noreply.github.com>
2020-12-24 19:05:16 +0700

    ทำ entity สำหรับเก็บข้อมูลชื่อผู้เเต่ง -close #50

---
+++ backend/ent/schema/author.go
@@ -0,0 +1,36 @@
+package schema
+
+import (
+    "github.com/facebookincubator/ent"
+    "github.com/facebookincubator/ent/schema/edge"
+    "github.com/facebookincubator/ent/schema/field"
+)
+
+
+// Author holds the schema definition for the Author entity.
+type Author struct {
+	ent.Schema
+}
+
+// Fields of the Author.
+func (Author) Fields() []ent.Field {
+	return []ent.Field{
+        field.String("Name").
+            NotEmpty().
+            Unique(),
+        field.String("Institutes").
+            NotEmpty(),
+        field.String("Position").
+            NotEmpty(),
+    }
+
+}
+
+// Edges of the Author.
+func (Author) Edges() []ent.Edge {
+	return []ent.Edge{
+        edge.To("owner", Research.Type).
+            StorageKey(edge.Column("OWNER_ID")),
+    }
+
+}
