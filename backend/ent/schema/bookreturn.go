d6b5b349f094c29122c0988e7b8b68f934e68411
NoT-Ton <varameth52@gmail.com>
2021-01-10 16:50:39 +0700

    update deadline in bookreturn - close #24

--- backend/ent/schema/bookreturn.go
+++ backend/ent/schema/bookreturn.go
@@ -15,26 +15,15 @@ type Bookreturn struct {
 // Fields of the Bookreturn.
 func (Bookreturn) Fields() []ent.Field {
 	return []ent.Field{
-<<<<<<< HEAD
-		field.Time("return_deadline"),
+		field.Time("DEADLINE"),
     }
-=======
-		field.String("book_name").NotEmpty(),
-	}
->>>>>>> c9cada08bc7f55755261c1e47704a41c6656a7a2
 }
 
 // Edges of the Bookreturn.
 func (Bookreturn) Edges() []ent.Edge {
 	return []ent.Edge{
-<<<<<<< HEAD
 		edge.From("user",User.Type).Ref("return").Unique(),
 		edge.From("location",Location.Type).Ref("returnfrom").Unique(),
 		edge.From("mustreturn", Bookborrow.Type).Ref("borrowed").Unique(),
-=======
-		edge.From("mustreturn", Bookborrow.Type).
-			Ref("borrowed").
-			Unique(),
->>>>>>> c9cada08bc7f55755261c1e47704a41c6656a7a2
 	}
 }
